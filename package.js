25225eed50b711b4900cdc9a8899935c5f84cca5
Lauricio Su <me@lauricio.com>
2015-10-05 12:39:26 +0700

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ox2:datetime-picker',
   summary: 'TESTING_DO_NOT_USE Datetime picker for meteor ',
-  version: '1.1.0',
+  version: '1.2.0',
   git: ' /* Fill me in! */ '
 });
 
