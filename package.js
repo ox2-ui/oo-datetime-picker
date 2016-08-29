89011292d17703bacdf51aa861fc7d9b6bdc44f3
Pauli B <pauli.buccini@gmail.com>
2016-08-29 15:24:22 +0700

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ox2:datetime-picker',
   summary: 'TESTING_DO_NOT_USE Datetime picker for meteor ',
-  version: '1.10.0',
+  version: '1.10.1',
   git: ' /* Fill me in! */ '
 });
 
